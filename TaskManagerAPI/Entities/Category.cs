7426c69b6e5e6aa5347943deeb6f07bad54c9707
JakubCzajor <jakub.czajor7@gmail.com>
2024-02-24 13:16:58 +0100

    Added Task and Category entities

---
+++ TaskManagerAPI/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManagerAPI.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual Task Task { get; set; }
+    }
+}
---
+++ TaskManagerAPI/Entities/Task.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TaskManagerAPI.Entities
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime LastModifiedDate { get; set;} = DateTime.Now;
+
+        public int CategoryId {  get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
