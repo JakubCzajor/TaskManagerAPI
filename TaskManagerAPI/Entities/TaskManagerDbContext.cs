ae131896cda5ab87717fec4d25a1412081f100b8
JakubCzajor <jakub.czajor7@gmail.com>
2024-02-24 13:53:14 +0100

    Added DbContext

---
+++ TaskManagerAPI/Entities/TaskManagerDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TaskManagerAPI.Entities
+{
+    public class TaskManagerDbContext : DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Task>()
+                .Property(t => t.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            modelBuilder.Entity<Task>()
+                .Property(t => t.Description)
+                .IsRequired()
+                .HasMaxLength(1000);
+
+            modelBuilder.Entity<Category>()
+                .Property(c => c.Name)
+                .IsRequired();
+        }
+    }
+}
