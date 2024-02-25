6725cee2e59a6a5d0330159de533d91733ccded6
JakubCzajor <jakub.czajor7@gmail.com>
2024-02-25 23:38:29 +0100

    Added new migration with fixed nullable fields

--- TaskManagerAPI/Entities/Task.cs
+++ TaskManagerAPI/Entities/Task.cs
@@ -7,8 +7,8 @@ namespace TaskManagerAPI.Entities
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public DateTime CreatedDate { get; set; } = DateTime.Now;
-        public DateTime LastModifiedDate { get; set;} = DateTime.Now;
+        public DateTime? CreatedDate { get; set; } = DateTime.Now;
+        public DateTime? LastModifiedDate { get; set;} = DateTime.Now;
 
         public int CategoryId {  get; set; }
         public virtual Category Category { get; set; }
--- TaskManagerAPI/Migrations/20240225222606_FixedNullableTaskFields.Designer.cs
+++ TaskManagerAPI/Migrations/20240225222606_FixedNullableTaskFields.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TaskManagerAPI.Entities;
 
@@ -11,9 +12,11 @@ using TaskManagerAPI.Entities;
 namespace TaskManagerAPI.Migrations
 {
     [DbContext(typeof(TaskManagerDbContext))]
-    partial class TaskManagerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240225222606_FixedNullableTaskFields")]
+    partial class FixedNullableTaskFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
