d0b0df2872eb0e6ef36160d59548ebcc612cfb07
JakubCzajor <jakub.czajor7@gmail.com>
2024-02-26 16:55:22 +0100

    Added mapping profile for AutoMapper

---
+++ TaskManagerAPI/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using TaskManagerAPI.Models;
+using TaskManagerAPI.Entities;
+
+namespace TaskManagerAPI
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Entities.Task, TaskDto>()
+                .ForMember(t => t.Category, c => c.MapFrom(s => s.Category.Name));
+        }
+    }
+}
